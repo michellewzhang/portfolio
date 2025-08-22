5ed0cefd7f6cb0cff8b519050e88a0640b0be803
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-08-22 01:51:32 -0700

    add play

---
+++ src/components/PlayGroup/PlayGroup.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './PlayGroup.css';
+
+const PlayGroup = ({ images, columns = 3, gap = '8px', mainPhotoIndex = null }) => {
+  return (
+    <div className="play-group">
+      <div 
+        className="play-group-gallery"
+        style={{
+          '--columns': columns,
+          '--gap': gap
+        }}
+      >
+        {images.map((image, index) => (
+          <div 
+            key={index} 
+            className={`play-group-image-container ${
+              mainPhotoIndex === index ? 'main-photo' : ''
+            }`}
+          >
+            <img
+              src={image.src}
+              alt={image.alt || `Image ${index + 1}`}
+              className="play-group-image"
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PlayGroup;
